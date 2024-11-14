93bd0835a94a67c3e085f67b1b25585195c9591c
Simone Marcato <simone@hevelop.com>
2024-11-14 12:57:04 +0100

    Generated client for service: order with version: 1.3.0 in language: python

--- order/configuration.py
+++ order/configuration.py
@@ -421,7 +421,7 @@ conf = order.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1\n"\
-               "SDK Package Version: 1.2.0".\
+               "SDK Package Version: 1.3.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
